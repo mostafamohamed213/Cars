aff52cafdad18b8daa0ec69e2e9b475bab46ae6a
mostafamohamed213 <64440025+mostafamohamed213@users.noreply.github.com>
2021-11-22 21:43:03 +0200

    project

---
+++ Cars/Models/CarsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class CarsContext : IdentityDbContext<ApplicationUser>
+    {
+        public CarsContext(DbContextOptions<CarsContext> options) : base(options)
+        {
+        }
+
+   
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
